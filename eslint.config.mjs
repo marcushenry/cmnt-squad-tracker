f702fac4ca173420bdec56e196100e3709ea73e2
Marcus Henry <marcusphenry@gmail.com>
2025-12-07 14:05:48 -0700

    Add lint step to CI pipeline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,20 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
+  // Next's core web vitals rules
   ...nextVitals,
+
+  // Next's TypeScript rules
   ...nextTs,
+
+  // Our custom rule overrides
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
