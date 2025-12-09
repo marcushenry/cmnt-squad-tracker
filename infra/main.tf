5ef6a5c3cf17e2a7a81aa6d60168e29c50ce8473
Marcus Henry <marcusphenry@gmail.com>
2025-12-09 16:13:42 -0700

    Add dev CloudFront + DNS config

--- infra/main.tf
+++ infra/main.tf
@@ -108,6 +108,83 @@ resource "aws_route53_record" "cmnt_dev" {
   }
 }
 
+############################################
+# CloudFront distribution for PROD
+#  - Origin: prod S3 website endpoint
+#  - Domain: cmnt.marcushenry.ca
+############################################
+
+resource "aws_cloudfront_distribution" "cmnt_prod" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  aliases = [
+    "cmnt.marcushenry.ca",
+  ]
+
+  origin {
+    # PROD S3 website endpoint
+    domain_name = aws_s3_bucket_website_configuration.site.website_endpoint
+    origin_id   = "cmnt-prod-s3-website-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "cmnt-prod-s3-website-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    compress = true
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    acm_certificate_arn      = data.aws_acm_certificate.marcushenry.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
+############################################
+# Route 53 record for cmnt.marcushenry.ca → CloudFront PROD
+############################################
+
+resource "aws_route53_record" "cmnt_prod" {
+  zone_id = data.aws_route53_zone.marcushenry.zone_id
+  name    = "cmnt"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cmnt_prod.domain_name
+    zone_id                = aws_cloudfront_distribution.cmnt_prod.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+
 
 variable "bucket_name" {
   type    = string
